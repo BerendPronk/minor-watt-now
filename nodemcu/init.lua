7d6eda69c0cf2683b7b3789b87c4a3f11712acfe
BerendPronk <berendpronk@quicknet.nl>
2017-05-11 01:14:40 +0200

    Add queue logic, add reset function on button press and provides file with code-comments

--- nodemcu/init.lua
+++ nodemcu/init.lua
@@ -1,80 +1,124 @@
+-- Defines used modules
+local config = require 'config'
 local wifimodule = require 'wifimodule'
 local socket = require 'socket'
-local config = require 'config'
 local timer = require 'timer'
 local servo = require 'servo'
 
+-- Initializes ESP8266 code, after Wi-Fi initialization
 function init()
+  -- Initializes socket connection
   local ws = socket.init()
 
+  -- Defines 'global' variables ( local within init() )
   local initFlex = nil
   local initLock = false
 
   local button = 1
   local isPressed = 1
 
-  local coinAmount = 0
+  local productPrice = nil
+  local coinInput = 0
+  local inQueue = 0
 
+  -- Defines servo opn GPIO-3 and sets it to a 1-degree angle
   servo.defineServo(3,0,1800)
   servo.setServo(3, 1)
 
-  timer.setInterval(function()
-    local flex = adc.read(0)
+  -- Checks type of socket message to run different functions
+  ws:on('receive', function(_, msg)
+    local data = cjson.decode(msg)
 
-    if initLock == false then
-      initFlex = flex
+    -- Sets custom average price and watches coin input afterwards
+    if data.type == 'assigned coinbox' then
+      productPrice = tonumber(data.avgPrice)
 
-      print('Initial flex: ' .. initFlex)
-
-      initLock = true
+      -- Runs watch function
+      watch()
     end
+  end)
 
-    if initFlex - flex >= 10 then
-      coinAmount = coinAmount + 1
-      print('Coin! ' .. coinAmount)
+  -- Runs a setinterval to read analog flex-sensor
+  function watch()
+    -- Sets interval of 50ms
+    timer.setInterval(function()
+      local flex = adc.read(0)
 
-      ok, json = pcall(cjson.encode, {
-        id = node.chipid(),
-        type = 'customer',
-        coins = coinAmount
-      })
-      if ok then
-        ws:send(json)
-      else
-        print('Failed to send data')
+      -- Sets idle flex after wifi-initialization, for better accuracy
+      if initLock == false then
+        initFlex = flex
+        print('Initial flex: ' .. initFlex)
+
+        -- Disables initFlex overwriting
+        initLock = true
       end
 
-      -- if (coinAmount == 3) then
-      --   coinAmount = 0
-      --
-      --   servo.setServo(3, 70)
-      --
-      --   timer.setTimeout(function()
-      --     servo.setServo(3, 1)
-      --   end, 1500)
-      -- end
-    end
-  end, 50)
+      -- Senses if coin has passed the flex-sensor
+      if initFlex - flex >= 10 then
+        coinInput = coinInput + 1
+        print('Coin! ' .. coinInput)
 
-  -- Dispose coin tray
+        -- Sends JSON over websocket when the input coins equals the average price
+        if coinInput >= productPrice then
+          inQueue = inQueue + 1
+
+          ok, json = pcall(cjson.encode, {
+            id = node.chipid(),
+            type = 'customer',
+            queue = inQueue
+          })
+          if ok then
+            ws:send(json)
+          else
+            print('Failed to send data')
+          end
+
+          coinInput = 0
+        end
+      end
+    end, 50)
+  end
+
+  -- Disposes coin tray
   function btnChange()
     if gpio.read(button) < isPressed then
         print('Open coin tray!')
-
         servo.setServo(3, 70)
 
-        -- Reset coin tray
+        -- Resets initial flex-sensor value
+        initFlex = adc.read(0)
+
+        -- Stops watch function
+        timer.stop()
+
+        -- Resets queue
+        inQueue = 0
+
+        -- Sends data to server via websocket
+        ok, json = pcall(cjson.encode, {
+          id = node.chipid(),
+          type = 'empty coinbox'
+        })
+        if ok then
+          ws:send(json)
+        else
+          print('Failed to send data')
+        end
+
+        -- Resets coin tray
         timer.setTimeout(function()
           print('Close coin tray!')
           servo.setServo(3, 1)
+
+          watch()
         end, 1500)
     end
   end
 
-  -- Initialize button GPIO
+  -- Initializes button GPIO
   gpio.mode(button, gpio.INT, gpio.PULLUP)
   gpio.trig(button, 'both', btnChange)
 end
 
--- Initilize Wi-fi connection
+-- Initilizes Wi-Fi connection
 wifimodule.connect(config, init)
