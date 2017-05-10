f2e9a663aad2dc9c799e8b7487d45546c21623ab
BerendPronk <berendpronk@quicknet.nl>
2017-05-10 15:42:10 +0200

    Add dummy participants data

--- public/data/participants.js
+++ public/data/participants.js
@@ -1,57 +1,50 @@
 const participants = {
   36451934: {
-    name: '',
-    product: '',
-    avgPrice: '',
-    avgWait: '',
+    name: 'Pizza by Pasquale',
+    product: 'Pizza',
+    avgPrice: 3,
     xPos: 187.06136540140662,
     yPos: 354.69712747266715
   },
   12235476: {
-    name: '',
-    product: '',
-    avgPrice: '',
-    avgWait: '',
+    name: 'Chandler\'s Chips',
+    product: 'Fries',
+    avgPrice: 2,
     xPos: 445.3499430133488,
     yPos: 539.6000925040576
   },
   98636553: {
-    name: '',
-    product: '',
-    avgPrice: '',
-    avgWait: '',
+    name: 'Faith\'s Frozen Yoghurts',
+    product: 'Ice Cream',
+    avgPrice: 1,
     xPos: 517.2966220138898,
     yPos: 288.5061827921694
   },
   75643973: {
-    name: '',
-    product: '',
-    avgPrice: '',
-    avgWait: '',
+    name: 'Scarlett Soup',
+    product: 'Soup',
+    avgPrice: 1,
     xPos: 628.8139744647284,
     yPos: 466.9339467135111
   },
   19857742: {
-    name: '',
-    product: '',
-    avgPrice: '',
-    avgWait: '',
+    name: 'Bane\'s Burgers',
+    product: 'Burgers',
+    avgPrice: 2,
     xPos: 702.1995870452802,
     yPos: 216.55950379162843
   },
   87063214: {
-    name: '',
-    product: '',
-    avgPrice: '',
-    avgWait: '',
+    name: 'Dale\'s Dogs',
+    product: 'Hot Dogs',
+    avgPrice: 2,
     xPos: 582.7680999043821,
     yPos: 137.4181568910333
   },
   55794326: {
-    name: '',
-    product: '',
-    avgPrice: '',
-    avgWait: '',
+    name: 'Cassidy\'s Clouds',
+    product: 'Cotton Candy',
+    avgPrice: 1,
     xPos: 625.2166405147013,
     yPos: 42.44854061031919
   }
