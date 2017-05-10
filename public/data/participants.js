7b9d908e2573772046268f2d010627803156a501
BerendPronk <berendpronk@quicknet.nl>
2017-05-10 15:59:31 +0200

    Add 'waiting' property to measure coin amount

--- public/data/participants.js
+++ public/data/participants.js
@@ -4,49 +4,56 @@ const participants = {
     product: 'Pizza',
     avgPrice: 3,
     xPos: 187.06136540140662,
-    yPos: 354.69712747266715
+    yPos: 354.69712747266715,
+    waiting: 0
   },
   12235476: {
     name: 'Chandler\'s Chips',
     product: 'Fries',
     avgPrice: 2,
     xPos: 445.3499430133488,
-    yPos: 539.6000925040576
+    yPos: 539.6000925040576,
+    waiting: 0
   },
   98636553: {
     name: 'Faith\'s Frozen Yoghurts',
     product: 'Ice Cream',
     avgPrice: 1,
     xPos: 517.2966220138898,
-    yPos: 288.5061827921694
+    yPos: 288.5061827921694,
+    waiting: 0
   },
   75643973: {
     name: 'Scarlett Soup',
     product: 'Soup',
     avgPrice: 1,
     xPos: 628.8139744647284,
-    yPos: 466.9339467135111
+    yPos: 466.9339467135111,
+    waiting: 0
   },
   19857742: {
     name: 'Bane\'s Burgers',
     product: 'Burgers',
     avgPrice: 2,
     xPos: 702.1995870452802,
-    yPos: 216.55950379162843
+    yPos: 216.55950379162843,
+    waiting: 0
   },
   87063214: {
     name: 'Dale\'s Dogs',
     product: 'Hot Dogs',
     avgPrice: 2,
     xPos: 582.7680999043821,
-    yPos: 137.4181568910333
+    yPos: 137.4181568910333,
+    waiting: 0
   },
   55794326: {
     name: 'Cassidy\'s Clouds',
     product: 'Cotton Candy',
     avgPrice: 1,
     xPos: 625.2166405147013,
-    yPos: 42.44854061031919
+    yPos: 42.44854061031919,
+    waiting: 0
   }
 }
 
